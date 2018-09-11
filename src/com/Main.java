7084b1cdb5c812ca8247e145d57b2bd44ffe5536
FrancoChen <850253432@qq.com>
2018-09-11 17:31:42 +0800

    'add'

---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Main {
+    public static void main(String[] args){
+        RBTree<String,String> rbTree = new RBTree<>();
+        rbTree.put("0707", "FrancoChen");
+        rbTree.put("0010", "Automan");
+        rbTree.inTraversal();
+        System.out.println("Get:" + rbTree.get("0010"));
+        System.out.println("Get:" + rbTree.get("0001"));
+    }
+}
--- src/com/RBTree.java
+++ src/com/RBTree.java
@@ -66,7 +66,22 @@ public class RBTree<Key extends Comparable<Key>,Value> {
             }
         }
         else{
-
+            if(isRed(node.right)){
+                if(isRed(node.right.left)){
+                    node.right = rightRotation(node.right);
+                }
+                if(isRed(node.right.right)){
+                    node = leftRotation(node);
+                }
+            }
+            else if(isRed(node.left)){
+                if(isRed(node.left.right)){
+                    node = leftRotation(node.left);
+                }
+                if(isRed(node.left.right)){
+                    node = rightRotation(node);
+                }
+            }
             if(isRed(node.left) && isRed(node.right) && !isRed(node)){
                 if(isRed(node.left.left) || isRed(node.left.right) || isRed(node.right.left) || isRed(node.right.right)){
                     flipColors(node);
@@ -76,12 +91,33 @@ public class RBTree<Key extends Comparable<Key>,Value> {
         return node;
     }
 
-    public void put(Key key,Value value){
+    public String get(Key key){
+        Node node = get(key, root);
+        return node == null ? "Null" : node.key + ":" +node.value;
+    }
+
+    private Node get(Key key, Node node){
+        if(node == null){
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if(cmp == 0){
+            return node;
+        }
+        else if(cmp > 0){
+            return get(key, node.right);
+        }
+        else{
+            return get(key, node.left);
+        }
+    }
+
+    public void put(Key key, Value value){
         root = put(key,value,root);
         root.color = BLACK;
     }
 
-    private Node put(Key key,Value value,Node node){
+    private Node put(Key key, Value value, Node node){
         if(node == null){
             size++;
             return new Node(key,value,null,null,RED);
@@ -97,7 +133,7 @@ public class RBTree<Key extends Comparable<Key>,Value> {
             else {
                 node.value = value;
             }
-            return node;
+            return balance(node);
         }
     }
 
@@ -129,5 +165,62 @@ public class RBTree<Key extends Comparable<Key>,Value> {
 
     public Node min(){
         return root == null ? null : min(root);
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isRBTree() {
+        return isRBTree(root);
+    }
+
+    public boolean isRBTree(Node node) {
+        if(node == null){
+            return true;
+        }else if(node.color == RED){
+            return false;
+        }else{
+            Node tnode;
+            int count = 0;
+            for(tnode = node;tnode != null;tnode = tnode.left){
+                if(tnode.color == BLACK){
+                    count++;
+                }
+            }
+            return isRBTree(node, count, 0);
+        }
+    }
+
+    private boolean isRBTree(Node node, int blacknum,int count){
+        if(node == null){
+            return blacknum == count ? true : false;
+        }else{
+            boolean flag = true;
+            if(node.color == BLACK) count++;
+            if(node.left != null)
+                flag &= isRBTree(node.left, blacknum, count);
+            if(node.right != null)
+                flag &= isRBTree(node.right, blacknum, count);
+            try {
+                flag &= !(isRBTree(node.left) && isRBTree(node.left.left));
+                flag &= !(isRBTree(node.left) && isRBTree(node.left.right));
+                flag &= !(isRBTree(node.right) && isRBTree(node.right.left));
+                flag &= !(isRBTree(node.right) && isRBTree(node.right.right));
+            }catch(NullPointerException e){
+                e.getStackTrace();
+            }
+            return flag;
+        }
+    }
+
+    public void inTraversal(){
+        inTraversal(root);
+    }
+
+    private void inTraversal(Node node){
+        if(node.left != null) inTraversal(node.left);
+        System.out.println(node.key + ":" + node.value);
+        if(node.right != null) inTraversal(node.right);
     }
 }
